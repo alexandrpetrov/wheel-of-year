5826bf61702ae639a11f6e015cdb5d085a38b97a
Alex Petrov <alex.petrov@omnigon.com>
2012-11-23 11:45:03 +0200

    Created empty project

---
+++ trunk/Wheel of Year/WYViewController.m
@@ -0,0 +1,29 @@
+//
+//  WYViewController.m
+//  Wheel of Year
+//
+//  Created by Administrator on 11/23/12.
+//  Copyright (c) 2012 Alexandr Petrov. All rights reserved.
+//
+
+#import "WYViewController.h"
+
+@interface WYViewController ()
+
+@end
+
+@implementation WYViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
